fbb15214b6b3735a1b9acc032ae9ddca2c1b68df
Julian Simioni <julian.simioni@gmail.com>
2012-10-23 22:11:11 -0700

    Add comments to test cases

--- stock/tests.py
+++ stock/tests.py
@@ -10,10 +10,12 @@ def test_easy_two_item_list():
     prices = [1, 2]
     assert_equal(maxProfit(prices), (0, 1))
 
+#test a case where the all time high price is not the best sell price
 def test_three_item_list():
     prices = [3, 1, 2]
     assert_equal(maxProfit(prices), (1, 2))
 
+#test a case where the all time low is not the best buy price
 def test_four_item_list():
     prices = [4, 6, 1, 2]
     assert_equal(maxProfit(prices), (0, 1))
