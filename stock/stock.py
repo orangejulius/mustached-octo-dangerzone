6bb7725e50c876965b8eaaca971cc23097460fc6
Julian Simioni <julian.simioni@gmail.com>
2012-10-23 21:06:08 -0700

    Keep track of the highest and lowest price seen so far
    Simply use these as the best sell and buy points

--- stock/stock.py
+++ stock/stock.py
@@ -1,4 +1,11 @@
 # given a list of past share prices, find the optimal time to buy and sell
 #return a tuple (best time to buy, best time to sell)
 def maxProfit(prices):
-    return (0,0)
+    lowestPriceIndex = highestPriceIndex = 0
+    for i, price in enumerate(prices):
+        if price > prices[highestPriceIndex]:
+            highestPriceIndex = i
+        if price < prices[lowestPriceIndex]:
+            lowestPriceIndex = i
+
+    return (lowestPriceIndex, highestPriceIndex)
