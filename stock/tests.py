f99767a81ff170bd5d535df884a7d338c82d6852
Julian Simioni <julian.simioni@gmail.com>
2012-10-23 21:05:49 -0700

    Test a simple size two case

--- stock/tests.py
+++ stock/tests.py
@@ -5,3 +5,7 @@ from stock import *
 def test_empty():
     prices = []
     assert_equal(maxProfit(prices), (0, 0))
+
+def test_easy_two_item_list():
+    prices = [1, 2]
+    assert_equal(maxProfit(prices), (0, 1))
