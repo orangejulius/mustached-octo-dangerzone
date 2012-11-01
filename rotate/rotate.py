275faf2a197f4669153f6c96f90c5eda2d6f6d18
Julian Simioni <julian.simioni@gmail.com>
2012-10-31 19:11:09 -0700

    Handle identical start/middle/end with linear search

--- rotate/rotate.py
+++ rotate/rotate.py
@@ -11,6 +11,12 @@ def compareSection(array, start, end):
     if end - start == 1:
         return end
     middle = (end + start)/2
+    #if the first and last elements are the same, binary
+    #style search won't work. just search linearly
+    if array[0] == array[-1] == array[middle]:
+        for i in range(len(array)-1):
+            if array[i] > array[i+1]:
+                return i+1
     #if the start of this section is greater than the middle,
     #the shift occurs in the first half of this subsection
     if (array[start] > array[middle]):
