75e2f4e5b6a5f22b64f497c556fe65f1cc937a16
Julian Simioni <julian.simioni@gmail.com>
2014-04-05 08:34:57 -0700

    Simplify logic into one if-else block
    It's now approaching readable

--- ruby/stock/stock.rb
+++ ruby/stock/stock.rb
@@ -8,18 +8,14 @@ class Stock
 
     buy_idx, sell_idx, lowest_price_idx = self.best_transaction_and_lowest(prices, n - 1)
 
-    if prices[n] < prices[lowest_price_idx]
-      lowest_price_idx = n
-    end
-
-    if prices[n] > prices[sell_idx]
-      sell_idx = n
-    end
-
     profit_so_far = prices[sell_idx] - prices[buy_idx]
     profit_since_lowest = prices[n] - prices[lowest_price_idx]
 
-    if profit_since_lowest > profit_so_far
+    if prices[n] < prices[lowest_price_idx]
+      return [buy_idx, sell_idx, n]
+    elsif prices[n] > prices[sell_idx]
+      return [lowest_price_idx, n, lowest_price_idx]
+    elsif profit_since_lowest > profit_so_far
       return [lowest_price_idx, n, lowest_price_idx]
     else
       return [buy_idx, sell_idx, lowest_price_idx]
