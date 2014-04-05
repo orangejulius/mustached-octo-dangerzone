1f0f66c51949dd23f7190cc660b2a9591a946492
Julian Simioni <julian.simioni@gmail.com>
2014-04-04 22:53:57 -0700

    Test case where highest sell price is not the best

--- ruby/stock/stock_test.rb
+++ ruby/stock/stock_test.rb
@@ -4,8 +4,8 @@ require 'minitest/autorun'
 require './stock'
 
 describe Stock do
-  it "returns nil on a one element array" do
-    Stock.best_transaction([5]).must_be_nil
+  it "returns [0, 0] on a one element array" do
+    Stock.best_transaction([5]).must_equal [0, 0]
   end
 
   it "returns [0, 1] with ascending array of size two" do
@@ -22,5 +22,9 @@ describe Stock do
 
   it "returns [2, 3] when a later buy opportunity is better with 4 elements" do
     Stock.best_transaction([2,3,1,4]).must_equal [2, 3]
+  end
+
+  it "returns [2, 3] when the highest sell price is not the best" do
+    Stock.best_transaction([4,5,1,3]).must_equal [2, 3]
   end
 end
